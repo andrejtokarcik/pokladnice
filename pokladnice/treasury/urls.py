99519809962e90f9474e9d1db3fb0bf1cb76e89d
Andrej Tokarčík <andrejtokarcik@gmail.com>
2008-08-31 19:57:48 +0200

    Primary implementation of user profiles

--- pokladnice/treasury/urls.py
+++ pokladnice/treasury/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from views import main
+from views import main, profile
 
 urlpatterns = patterns('',
     (r'^$', main),
+    (r'^uzivatel/(?P<username>[a-zA-Z0-9._-]+)', profile)
 )
--- pokladnice/treasury/views.py
+++ pokladnice/treasury/views.py
@@ -4,3 +4,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def main(request):
     return render_with_context(request, 'home.html')
+
+@login_required
+def profile(request, username):
+    return render_with_context(request, 'profile.html', {'username': username})
