2f9828d4a40153ad5d3c3014361ccd1274cda84e
Andrej Tokarčík <andrejtokarcik@gmail.com>
2008-09-10 19:31:43 +0200

    Applying storage limit setting

--- pokladnice/treasury/views.py
+++ pokladnice/treasury/views.py
@@ -9,25 +9,40 @@ def main(request):
 
 @login_required
 def upload(request):
+    errors = ''
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            from os import mkdir
-            from os.path import join as path_join, exists
+            from os import mkdir, walk
+            from os.path import exists, getsize, join
 
-            destination = path_join(settings.STORAGE_LOCATION,
+            destination = join(settings.STORAGE_LOCATION,
                     request.user.username)
             if not exists(destination):
                 mkdir(destination)
 
-            destination = open(path_join(destination,
-                request.FILES['file'].name), 'wb+')
+            def get_dir_size(directory):
+                """Get size of directory contents"""
+                dir_size = 0
+                for path, dirs, files in walk(directory):
+                    for file in files:
+                        dir_size += getsize(join(path, file))
+                    for dir in dirs:
+                        dir_size += get_dir_size(join(path, dir))
+                return dir_size
 
-            for chunk in request.FILES['file'].chunks():
-                destination.write(chunk)
+            if (get_dir_size(destination) + request.FILES['file'].size) > (settings.STORAGE_LIMIT * 1024 ** 2):
+                errors = 'Limit dosiahnuty\n'
+            else:
+                destination = open(join(destination,
+                    request.FILES['file'].name), 'wb+')
+
+                for chunk in request.FILES['file'].chunks():
+                    destination.write(chunk)
     else:
         form = UploadFileForm()
-    return render_with_context(request, 'upload.html', {'form': form})
+    return render_with_context(request, 'upload.html',
+            {'form': form, 'errors': errors})
 
 @login_required
 def profile(request, username):
