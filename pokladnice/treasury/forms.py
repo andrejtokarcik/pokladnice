64160a0fad85a1875de4c05bc7af26a9cb8554ea
Andrej Tokarčík <andrejtokarcik@gmail.com>
2008-10-09 18:54:17 +0200

    Introducing internationalization (in forms & templates)

--- pokladnice/treasury/forms.py
+++ pokladnice/treasury/forms.py
@@ -1,6 +1,6 @@
-# -*- encoding: utf-8 -*-
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 class UploadFileForm(forms.Form):
-    #title = forms.CharField(label="Názov", max_length=50)
-    file  = forms.FileField(label="Súbor")
+    #title = forms.CharField(label=_('Title'), max_length=50)
+    file  = forms.FileField(label=_('File path'))
