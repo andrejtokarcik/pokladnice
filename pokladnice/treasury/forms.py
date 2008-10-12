7c3a51cd7b2e5eb449bdf9b0440b1cf5c79dfcc1
Andrej Tokarčík <andrejtokarcik@gmail.com>
2008-10-12 16:18:22 +0200

    File upload form is generated from model & automatically checks the storage limit

--- pokladnice/treasury/forms.py
+++ pokladnice/treasury/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.core.files.storage import default_storage as storage
 from django.utils.translation import ugettext_lazy as _
 
-class UploadFileForm(forms.Form):
-    #title = forms.CharField(label=_('Title'), max_length=50)
-    file  = forms.FileField(label=_('File path'))
+from models import UploadedFile
+
+class UploadFileForm(forms.ModelForm):
+    """
+    A form that handles the most basic upload process.
+    """
+
+    class Meta:
+        model = UploadedFile
+        fields = ['file']
+
+    def clean_file(self):
+        """
+        Check file size regarding the storage limit.
+        """
+        newsize = storage.get_used_space() + self.cleaned_data['file'].size
+        if newsize > storage.limit:
+            raise forms.ValidationError(_('Storage limit exceeded'))
+        else:
+            return self.cleaned_data['file']
+
--- pokladnice/treasury/models.py
+++ pokladnice/treasury/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 def get_file_path(instance, filename):
     from os.path import join
     return join(instance.user.username, filename)
 
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to=get_file_path)
-    size = models.IntegerField()
+    file = models.FileField(_('File path'), upload_to=get_file_path)
     user = models.ForeignKey(User)
+    size = models.IntegerField()
